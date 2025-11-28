2289739f4ad7494212a7f5d09f51567f89d31267
jason <jasonyee1999@gmail.com>
2025-11-28 17:35:49 +0800

    multipage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-outlier_page = st.Page("pages\outlier_demo.py", title="离散点DEMO")
-merge_csv_page = st.Page("pages\merge_csv.py", title="Merge CSV")
+outlier_page = st.Page("outlier_demo.py", title="离散点DEMO")
+merge_csv_page = st.Page("merge_csv.py", title="Merge CSV")
 
 pg = st.navigation([outlier_page, merge_csv_page])
 st.set_page_config(page_title="Data manager")
