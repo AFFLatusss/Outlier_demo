60d79c4f0e43cd6dcbdb7dbb98bfca4561286953
AFFLatusss <jasonyee1999@gmail.com>
2025-12-12 20:21:03 +0800

    bpm search

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 outlier_page = st.Page("outlier_demo.py", title="离散点DEMO")
 merge_csv_page = st.Page("merge_csv.py", title="Merge CSV")
+bpm_resign_search = st.Page("bpm_resign_search.py", title="BPM离职账号查询")
 
-pg = st.navigation([outlier_page, merge_csv_page])
+pg = st.navigation([outlier_page, merge_csv_page, bpm_resign_search])
 st.set_page_config(page_title="Data manager")
 pg.run()
